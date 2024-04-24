8fbf5c3afc26c53ab2096f4ea2e9492780de7d9e
Kimjinsu <145655175+Tyrano1129@users.noreply.github.com>
2024-04-24 15:52:01 +0900

    update
    update

--- resources/script/index.js
+++ resources/script/index.js
@@ -27,72 +27,72 @@ headers.forEach((header) => {
   });
 });
 
-const backendDiv = document.querySelector(".backend");
-const frontendDiv = document.querySelector(".frontend");
+// const backendDiv = document.querySelector(".backend");
+// const frontendDiv = document.querySelector(".frontend");
 
-function Project(link, imageUrl, title, description, skills, period) {
-  this.link = link;
-  this.imageUrl = imageUrl;
-  this.title = title;
-  this.description = description;
-  this.skills = skills;
-  this.period = period;
-}
+// function Project(link, imageUrl, title, description, skills, period) {
+//   this.link = link;
+//   this.imageUrl = imageUrl;
+//   this.title = title;
+//   this.description = description;
+//   this.skills = skills;
+//   this.period = period;
+// }
 
-function addProject(project, targetContainer, imageSize, imagePosition) {
-  let container;
-  if (targetContainer === "backend") {
-    container = backendDiv;
-  } else {
-    container = frontendDiv;
-  }
+// function addProject(project, targetContainer, imageSize, imagePosition) {
+//   let container;
+//   if (targetContainer === "backend") {
+//     container = backendDiv;
+//   } else {
+//     container = frontendDiv;
+//   }
 
-  const article = document.createElement("article");
-  const a = document.createElement("a");
-  const projectImg = document.createElement("div");
-  const title = document.createElement("h3");
-  const description = document.createElement("h6");
-  const skills = document.createElement("div");
-  const period = document.createElement("span");
+//   const article = document.createElement("article");
+//   const a = document.createElement("a");
+//   const projectImg = document.createElement("div");
+//   const title = document.createElement("h3");
+//   const description = document.createElement("h6");
+//   const skills = document.createElement("div");
+//   const period = document.createElement("span");
 
-  article.setAttribute("class", project.title);
-  a.setAttribute("href", project.link);
-  a.setAttribute("target", "_blank");
-  projectImg.setAttribute("class", "project-img");
-  projectImg.setAttribute(
-    "style",
-    `background: no-repeat ${imagePosition} url('${project.imageUrl}'); background-size: ${imageSize}`
-  );
-  title.setAttribute("class", "title");
-  title.innerText = project.title;
-  description.className = "description";
-  description.innerText = project.description;
-  skills.setAttribute("class", "skills");
-  period.className = "period";
-  period.innerText = `${project.period[0]} ~ ${project.period[1]}`;
+//   article.setAttribute("class", project.title);
+//   a.setAttribute("href", project.link);
+//   a.setAttribute("target", "_blank");
+//   projectImg.setAttribute("class", "project-img");
+//   projectImg.setAttribute(
+//     "style",
+//     `background: no-repeat ${imagePosition} url('${project.imageUrl}'); background-size: ${imageSize}`
+//   );
+//   title.setAttribute("class", "title");
+//   title.innerText = project.title;
+//   description.className = "description";
+//   description.innerText = project.description;
+//   skills.setAttribute("class", "skills");
+//   period.className = "period";
+//   period.innerText = `${project.period[0]} ~ ${project.period[1]}`;
 
-  a.append(projectImg);
-  article.append(a);
-  article.append(title);
-  article.append(description);
-  project.skills.forEach((str) => {
-    const skill = document.createElement("span");
-    skill.innerText = str;
-    skills.append(skill);
-  });
-  article.append(skills);
-  article.append(period);
+//   a.append(projectImg);
+//   article.append(a);
+//   article.append(title);
+//   article.append(description);
+//   project.skills.forEach((str) => {
+//     const skill = document.createElement("span");
+//     skill.innerText = str;
+//     skills.append(skill);
+//   });
+//   article.append(skills);
+//   article.append(period);
 
-  container.append(article);
-}
+//   container.append(article);
+// }
 
-const bookSearch = new Project(
-  "/booksearch",
-  "https://em-content.zobj.net/thumbs/240/facebook/65/left-pointing-magnifying-glass_1f50d.png",
-  "Book Search",
-  "Book Search and Inquiry Service",
-  ["VanilaJS", "jQeury", "Kakao Search API"],
-  ["2024.04.24", "2024.04.24"]
-);
+// const bookSearch = new Project(
+//   "/booksearch",
+//   "https://em-content.zobj.net/thumbs/240/facebook/65/left-pointing-magnifying-glass_1f50d.png",
+//   "Book Search",
+//   "Book Search and Inquiry Service",
+//   ["VanilaJS", "jQeury", "Kakao Search API"],
+//   ["2024.04.24", "2024.04.24"]
+// );
 
-addProject(bookSearch, "frontend", "contain", "center");
+// addProject(bookSearch, "frontend", "contain", "center");
